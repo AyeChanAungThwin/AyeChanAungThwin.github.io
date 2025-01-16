e3f45f19bbf1c77e7dae4851fc6fa497bd8ba763
ayechanaungthwin <ayechanaungthwin@gmail.com>
2025-01-16 20:51:40 +0700

    added DOB

--- js/index.js
+++ js/index.js
@@ -10,3 +10,28 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+function calculateAge(dob) {
+    const dobDate = new Date(dob);
+    const currentDate = new Date();
+    let age = currentDate.getFullYear() - dobDate.getFullYear();
+    const monthDifference = currentDate.getMonth() - dobDate.getMonth();
+    const dayDifference = currentDate.getDate() - dobDate.getDate();
+
+    if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+        age--;
+    }
+
+    return age;
+}
+
+function appendAgeIntoPTag(dob) {
+    const age = calculateAge(dob);
+    const pTag = document.querySelector('.section__subtitle.section__subtitle--age');
+    if (pTag) {
+        pTag.textContent += ` (Age: ${age})`;
+    }
+}
+
+const dob = '1995-01-01'; //Not my real DOB for securing my personal info but my actual birth year!
+appendAgeIntoPTag(dob);
